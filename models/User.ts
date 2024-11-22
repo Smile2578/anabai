94ac264bc66ad98bc72953b5b05af71d623f4329
Smile <kravcss@gmail.com>
2024-11-22 18:25:57 +0100

    bcrypt user model

--- models/User.ts
+++ models/User.ts
@@ -1,7 +1,7 @@
 // models/User.ts
 
 import mongoose, { Schema, Document, Model } from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 export interface IUser extends Document {
   _id: mongoose.Types.ObjectId;
