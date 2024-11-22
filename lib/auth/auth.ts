8917b7cc38cbb153c5cf73c9e7e1f6bed38ef0b7
Smile <kravcss@gmail.com>
2024-11-22 20:31:28 +0100

    manual get token

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -75,10 +75,21 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: 'jwt',
-    maxAge: 30 * 24 * 60 * 60
+    maxAge: 30 * 24 * 60 * 60, // 30 jours
   },
   jwt: {
-    maxAge: 30 * 24 * 60 * 60,
     secret: process.env.NEXTAUTH_SECRET,
-  }
+    maxAge: 30 * 24 * 60 * 60, // 30 jours
+  },
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        path: '/',
+      },
+    },
+  },
 };
--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
   const secret = process.env.NEXTAUTH_SECRET;
+  
+  // Tentez de récupérer le token
   const token = await getToken({ req, secret });
 
   console.log('Middleware token:', token);
 
   if (req.nextUrl.pathname.startsWith('/admin')) {
-    // Si l'utilisateur n'est pas connecté, rediriger vers la page de connexion
     if (!token) {
       console.log('User not authenticated');
       return NextResponse.redirect(new URL('/auth/signin', req.url));
     }
 
-    // Si l'utilisateur n'est pas admin, rediriger vers la page d'accueil
     if (token.role !== 'admin') {
       console.log('Access denied. Role:', token.role);
       return NextResponse.redirect(new URL('/', req.url));
@@ -27,5 +27,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/admin/:path*'], // Appliquer le middleware aux routes /admin
+  matcher: ['/admin/:path*'],
 };
