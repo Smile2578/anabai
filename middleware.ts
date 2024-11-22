d1d48227bdf705278e47284a96fd7628d056f001
Smile <kravcss@gmail.com>
2024-11-22 20:27:21 +0100

    middleware gettoken

--- middleware.ts
+++ middleware.ts
@@ -1,42 +1,31 @@
-import { withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
-export default withAuth(
-  async function middleware(req) {
-    let token = req.nextauth.token;
+export async function middleware(req: NextRequest) {
+  const secret = process.env.NEXTAUTH_SECRET;
+  const token = await getToken({ req, secret });
 
+  console.log('Middleware token:', token);
+
+  if (req.nextUrl.pathname.startsWith('/admin')) {
+    // Si l'utilisateur n'est pas connecté, rediriger vers la page de connexion
     if (!token) {
-      console.log('Token not found in req.nextauth. Fetching manually...');
-      token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+      console.log('User not authenticated');
+      return NextResponse.redirect(new URL('/auth/signin', req.url));
     }
 
-    console.log('Middleware token:', token);
-
-    if (req.nextUrl.pathname.startsWith('/admin')) {
-      if (!token) {
-        console.log('User not authenticated');
-        return NextResponse.redirect(new URL('/auth/signin', req.url));
-      }
-
-      if (token.role !== 'admin') {
-        console.log('Access denied. Role:', token.role);
-        return NextResponse.redirect(new URL('/', req.url));
-      }
-
-      console.log('Access granted. Role:', token.role);
+    // Si l'utilisateur n'est pas admin, rediriger vers la page d'accueil
+    if (token.role !== 'admin') {
+      console.log('Access denied. Role:', token.role);
+      return NextResponse.redirect(new URL('/', req.url));
     }
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => {
-        console.log('Authorized callback token:', token);
-        return token?.role === 'admin';
-      },
-    },
+
+    console.log('Access granted. Role:', token.role);
   }
-);
+
+  return NextResponse.next(); // Continue la requête normalement
+}
 
 export const config = {
-  matcher: ['/admin/:path*'],
+  matcher: ['/admin/:path*'], // Appliquer le middleware aux routes /admin
 };
